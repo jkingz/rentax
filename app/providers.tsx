a4941f3309e5bc75a3dfb7d809d12ff28ee0a55a
jkingz <jfkrule@gmail.com>
2025-04-18 17:18:05 +0800

    feat: Add auth provider page and redux state

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,15 @@
 'use client';
 
 import StoreProvider from '@/state/redux';
-// import { Authenticator } from "@aws-amplify/ui-react";
-// import Auth from "./(auth)/authProvider";
+import { Authenticator } from '@aws-amplify/ui-react';
+import Auth from './(auth)/authProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      {/* <Authenticator.Provider>
+      <Authenticator.Provider>
         <Auth>{children}</Auth>
-      </Authenticator.Provider> */}
-      {children}
+      </Authenticator.Provider>
     </StoreProvider>
   );
 };
--- state/api.ts
+++ state/api.ts
@@ -1,12 +1,65 @@
+import { createNewUserInDatabase } from '@/lib/utils';
+import { Manager, Tenant } from '@/types/prismaTypes';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+      if (idToken) {
+        headers.set('Authorization', `Bearer ${idToken}`);
+      }
+      return headers;
+    },
   }),
   reducerPath: 'api',
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const user = await getCurrentUser();
+          const userRole = idToken?.payload['custom:role'] as string;
+
+          const endpoint =
+            userRole === 'manager'
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let userDetailsResponse = await fetchWithBQ(endpoint);
+
+          //check if user exists, create if not
+          if (
+            userDetailsResponse.error &&
+            userDetailsResponse.error.status === 404
+          ) {
+            userDetailsResponse = await createNewUserInDatabase(
+              user,
+              idToken,
+              userRole,
+              fetchWithBQ,
+            );
+          }
+
+          //create user if doesn't exist
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole,
+            } as User,
+          };
+        } catch (error: any) {
+          return { error: error.message || 'Something went wrong' };
+        }
+      },
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetAuthUserQuery } = api;
