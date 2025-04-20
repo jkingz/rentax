9db5db9f9ea11fa30b2c675ef2a6176ebfbbd60c
jkingz <jfkrule@gmail.com>
2025-04-21 03:29:21 +0800

    feat: Add lang hero section page

--- app/(nondashboard)/landing/HeroSection.tsx
+++ app/(nondashboard)/landing/HeroSection.tsx
@@ -1,19 +1,54 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { setFilters } from '@/state';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const HeroSection = () => {
+  const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState('');
-  const handleLocationSearch = () => {
-    // Handle location search logic here
+  const router = useRouter();
+
+  const handleLocationSearch = async () => {
+    try {
+      const trimmedQuery = searchQuery.trim();
+      if (!trimmedQuery) return;
+
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          trimmedQuery,
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`,
+      );
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+        dispatch(
+          setFilters({
+            location: trimmedQuery,
+            coordinates: [lng, lat],
+          }),
+        );
+        const params = new URLSearchParams({
+          location: trimmedQuery,
+          lat: lat.toString(),
+          lng: lng.toString(),
+        });
+        router.push(`/search?${params.toString()}`);
+      }
+    } catch (error) {
+      console.error('error search location:', error);
+    }
   };
   return (
     <div className="relative h-screen">
       <Image
-        src="/landing-splash.jpg"
+        src="/landing-splash2.avif"
         alt="landing image"
         fill
         priority
